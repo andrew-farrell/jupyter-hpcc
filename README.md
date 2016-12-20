76110c26b3c0fc2902fe89e230803a32e03e5ad1
afarrell <andrew.dublin@gmail.com>
2016-12-20 00:10:31 +0000

    code and docs tidy

--- README.md
+++ README.md
@@ -5,11 +5,36 @@
 
 Allows HPCC's ECL to be run in each Jupyter cell
 
+ECLPLUS used to send ECL to HPCC
+
 Local python variables can be bound to placeholders in ECL
 
 Allows polygot development of solutions using ECL along with python and any other embedded languages in one Jupyter notebook.
 e.g. Transform your data at scale with HPCC using ECL then visualise the results using Python and matplotlib
 
-## TODO
+## How to raise issues:
+* https://github.com/andrew-farrell/jupyter-hpcc/issues
+
+## Prerequsites:
+* Install https://hpccsystems.com/download/developer-tools/client-tools
+** make sure eclplus is available on the path envinronment var
+* Python 2.7.X
+* Jupyter, http://jupyter.org/ or via Anaconda  https://www.continuum.io/downloads
+* HPCC virtual machine (https://hpccsystems.com/download/virtual-machine-image) or have HPCC installed already
+
+## Examples:
+*  https://github.com/andrew-farrell/jupyter-hpcc/jupyter-hpcc-first-notebook.ipynb
+
+## TODO:
 * integrate with hpcc visualisation library
-* allow easy ingestion of data 
+* allow easy ingestion of data e.g. spraying of csv into HPCC
+* create tighter integration with ECPLUS or SOAP services to give more subtle control
+* create tighter integration with pandas
+* make it easier to handle outputs
+* move to asychronous mechansim, poll workunit instead
+
+## Credits:
+
+## hpcc magic based on code from:
+* https://github.com/catherinedevlin/ipython-sql
+* https://github.com/abingham/boost_python_tutorial
--- hpcc_magic.py
+++ hpcc_magic.py
@@ -12,17 +12,7 @@ except ImportError:
     DataFrame = None
     Series = None
 
-# from sqlalchemy.exc import ProgrammingError, OperationalError
-
-# import sql.connection
-# import sql.parse
-# import sql.run
-
-
-
 from collections import defaultdict
-
-import xmltodict
 
 @magics_class
 class HpccMagic(Magics, Configurable):
@@ -30,17 +20,7 @@ class HpccMagic(Magics, Configurable):
 
     Provides the %%hpcc magic."""
 
-    autolimit = Int(0, config=True, help="Automatically limit the size of the returned result sets")
-    style = Unicode('DEFAULT', config=True, help="Set the table printing style to any of prettytable's defined styles (currently DEFAULT, MSWORD_FRIENDLY, PLAIN_COLUMNS, RANDOM)")
-    short_errors = Bool(True, config=True, help="Don't display the full traceback on SQL Programming Error")
-    displaylimit = Int(0, config=True, help="Automatically limit the number of rows displayed (full result set is still stored)")
-    autopandas = Bool(False, config=True, help="Return Pandas DataFrames instead of regular result sets")
-    column_local_vars = Bool(False, config=True, help="Return data into local variables from column names")
-    feedback = Bool(True, config=True, help="Print number of rows affected by DML")
-    dsn_filename = Unicode('odbc.ini', config=True, help="Path to DSN file. "
-                           "When the first argument is of the form [section], "
-                           "a sqlalchemy connection string is formed from the "
-                           "matching section in the DSN file.")
+
                            
   
 
@@ -54,36 +34,20 @@ class HpccMagic(Magics, Configurable):
         self.shell.configurables.append(self)
 
     @needs_local_scope
-    @line_magic('hpcc')
+    #@line_magic('hpcc')
     @cell_magic('hpcc')
     def execute(self, line, cell='', local_ns={}):
-        """Runs SQL statement against a database, specified by SQLAlchemy connect string.
-
-        If no database connection has been established, first word
-        should be a SQLAlchemy connection string, or the user@db name
-        of an established connection.
-
-        Examples::
-
-          %%sql postgresql://me:mypw@localhost/mydb
-          SELECT * FROM mytable
-
-          %%sql me@mydb
-          DELETE FROM mytable
-
-          %%sql
-          DROP TABLE mytable
-
-        SQLAlchemy connect string syntax examples:
-
-          postgresql://me:mypw@localhost/mydb
-          sqlite://
-          mysql+pymysql://me:mypw@localhost/mydb
+        """ 
+        
+        Runs HPCC ECL
 
         """
         # save globals and locals so they can be referenced in bind vars
         user_ns = self.shell.user_ns.copy()
         user_ns.update(local_ns)
+        
+        def param_details():
+            return 'Usage %%hpcc [cell_name] user=[user] password=[password] server=[server] cluster=[cluster]'
         
         def replace_local(input):
             result = input
@@ -91,9 +55,7 @@ class HpccMagic(Magics, Configurable):
             #return re.sub(r"[%]\s*[^%]+\s*[%]",lambda k:self.shell.user_ns.get(k[1,-1].strip(),k),input)
             for v in re.findall(r"%(\w+)%",input):
                 result = re.sub(r'%\s?' + v + '\s?%', str(self.shell.user_ns[str(v)]), result)
-                # local_var = str(self.shell.user_ns[str(v)])
-                # result = local_var
-                # #result = re.sub(r'\b'+str(v)+'\b', local_var, result)
+                
             return result
             
         p_line = replace_local(line)
@@ -104,20 +66,8 @@ class HpccMagic(Magics, Configurable):
         name = args[0]
         
 
-        # args = args[1:]
-        # debug = 'debug' in args
-
         # Define the source and executable filenames.
         source_filename = '{}.ecl'.format(name)
-
-        # parsed = sql.parse.parse('%s\n%s' % (line, cell), self)
-        # conn = sql.connection.Connection.get(parsed['connection'])
-        # first_word = parsed['hpcc'].split(None, 1)[:1]
-        # if first_word and first_word[0].lower() == 'persist':
-            # return self._persist_dataframe(parsed['hpcc'], conn, user_ns)
-            
-
-            
 
         try:
             p_cell = replace_local(cell)
@@ -127,7 +77,7 @@ class HpccMagic(Magics, Configurable):
 
             result = []
 
-            # Compile the C++ code into an executable.
+            # Prepare ECL for use with ECLPLUS
             cmd = "eclplus {} {} {} {} {} {} {} {}".format(
                 'action=query',
                 'user='+parsed_args['user'],
@@ -138,68 +88,25 @@ class HpccMagic(Magics, Configurable):
                 'format=xml',
                 'output=' + '{}.xml'.format(source_filename))
 
-            # if debug:
-            # result.append(cmd)
-
+        
             compile = self.shell.getoutput(cmd)
-            #result = genfromtxt('{}.csv'.format(name), delimiter=',')
-            #output = self.shell.getoutput(program_filename)
-            # result.append(output)
-            # from xml.etree.ElementTree import ElementTree
-            # tree = ElementTree()
-            # test = tree.parse('{}.xml'.format(name))
-            #root = tree.getroot()
-            f = open('{}.xml'.format(source_filename), "r") #convert('{}.xml'.format(name))
+         
+            f = open('{}.xml'.format(source_filename), "r") 
             result = f.read()
             f.close()
             
-            return result #self.shell.user_ns[re.findall(r"%(\w+)%",p_line)[0]]
-            
-            # result = sql.run.run(conn, parsed['hpcc'], self, user_ns)
+            return result 
 
-            # if result is not None and ~isinstance(result, str) and self.column_local_vars:
-                # #Instead of returning values, set variables directly in the
-                # #users namespace. Variable names given by column names
-
-                # if self.autopandas:
-                    # keys = result.keys()
-                # else:
-                    # keys = result.keys
-                    # result = result.dict()
-
-                # if self.feedback:
-                    # print('Returning data to local variables [{}]'.format(
-                        # ', '.join(keys)))
-
-                # self.shell.user_ns.update(result)
-
-                # return None
-            # else:
-                # #Return results into the default ipython _ variable
-                # return result
-
-        except (RuntimeError) as e:
-            # Sqlite apparently return all errors as OperationalError :/
+        except (RuntimeError) as rune:
             if self.short_errors:
-                print(e)
+                print(rune)
             else:
                 raise
+        except (NameError) as ne:
+            return param_details()
+        except (KeyError) as ke:
+            return param_details()
 
-    # legal_sql_identifier = re.compile(r'^[A-Za-z0-9#_$]+')
-    # def _persist_dataframe(self, raw, conn, user_ns):
-        # if not DataFrame:
-            # raise ImportError("Must `pip install pandas` to use DataFrames")
-        # pieces = raw.split()
-        # if len(pieces) != 2:
-            # raise SyntaxError("Format: %sql [connection] persist <DataFrameName>")
-        # frame_name = pieces[1].strip(';')
-        # frame = eval(frame_name, user_ns)
-        # if not isinstance(frame, DataFrame) and not isinstance(frame, Series):
-            # raise TypeError('%s is not a Pandas DataFrame or Series' % frame_name)
-        # table_name = frame_name.lower()
-        # table_name = self.legal_sql_identifier.search(table_name).group(0)
-        # frame.to_sql(table_name, conn.session.engine)
-        # return 'Persisted %s' % table_name
 
 
 def load_ipython_extension(ip):
